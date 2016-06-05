66844f189aa4a5dacb389450412ac54993b3f5bb
Kevin_Nav <kvin11@hotmail.com>
2016-06-05 14:50:18 -0600

    new mod

--- appPrueba/routes/index.js
+++ appPrueba/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Examen I Parcial ' });
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto', { title: 'Contacto' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
