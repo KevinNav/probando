a8678d94786e5844d01151896c1e614fc452786f
Kevin_Nav <kvin11@hotmail.com>
2016-06-07 00:51:12 -0600

    new mod...completes

--- appPrueba/routes/index.js
+++ appPrueba/routes/index.js
@@ -15,4 +15,22 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
 
+router.post('/contacto', function(req, res, next){
+  console.log(req.body);
+  var nombre = " ";
+
+  if (req.body.btnEnviar && true){
+
+    if (req.body.txtNombre === "Kevin"){
+      res.redirect('/');
+    }
+    console.log("ENTRO");
+    nombre = String(req.body.txtNombre);
+    console.log(nombre);
+  }
+
+  req.body.nombre = nombre;
+  res.render('contacto', req.body);
+});
+
 module.exports = router;
