27637682bdc86a3a350a72ec951b850659f81491
Kevin_Nav <kvin11@hotmail.com>
2016-06-07 00:56:57 -0600

    exress

--- appPrueba/routes/index.js
+++ appPrueba/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Examen I Parcial ' });
+  res.render('index', { title: 'Express ' });
 });
 
 router.get('/contacto', function(req, res, next) {
